63fcc1b56a3c1d75328a9f0bb9dc94ad6bcae724
Kajenthini Sathiyaseelathevan <ksathiyaseelathev@mail.valenciacollege.edu>
2016-02-08 14:16:00 -0500

    week 4

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,4 +1,10 @@
 def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+    
     # TO DO #################################################################
     # Write code in this function that prints the game board.               #
     # The code in this function should only print, the user should NOT      #
@@ -9,6 +15,18 @@ def printBoard(board):
 
 def checkWinner(board, player):    
     print('Checking if ' + player + ' is a winner...')
+    player=input()
+
+winner = ((board['top-L']== player and board['top-M']== player and board['top-R']== player or
+           board['mid-L']== player and board['mid-M']== player and board['mid-R']== player or
+           board['low-L']== player and board['low-M']== player and board['low-R']== player or
+           board['top-L']== player and board['mid-L']== player and board['low-L']== player or
+           board['top-M']== player and board['mid-M']== player and board['low-M']== player or
+           board['top-R']== player and board['mid-R']== player and board['low-R']== player or
+           board['top-L']== player and board['mid-M']== player and board['low-R']== player or
+           board['top-R']== player and board['mid-M']== player and board['low-L']== player))
+return winner
+    
     
     # TO DO #################################################################
     # Write code in this function that checks the tic-tac-toe board          #
@@ -27,22 +45,22 @@ def startGame(startingPlayer, board):
     #########################################################################
 
     turn = startingPlayer
-    for i in range(9):
-        printBoard(board)
-        print('Turn for ' + turn + '. Move on which space?')
-        move = input()
-        board[move] = turn
-        if( checkWinner(board, 'X') ):
-            print('X wins!')
-            break
-        elif ( checkWinner(board, 'O') ):
-            print('O wins!')
-            break
+    for i in range(9): # 9 for loop for 9 (0-8) spaces
+        printBoard(board) # calling the function
+        print('Turn for ' + turn + '. Move on which space?') # print function output on thw screen
+        move = input() # user will input their move
+        board[move] = turn # computer's move
+        if( checkWinner(board, 'X') ): #if clause
+            print('X wins!') # output on the screen, tells who wins
+            break # break function to break the function
+        elif ( checkWinner(board, 'O') ): # elif statement 
+            print('O wins!') # output prints in the screen who wins
+            break  # break the function
     
         if turn == 'X':
             turn = 'O'
         else:
             turn = 'X'
         
-    printBoard(board)
+    printBoard(board) # calling the function. 
     
